25b6a88b155f7cf9c0b282e95cd71bf5b072977b
Corintho Assunção <corintho.filho@gmail.com>
2022-06-23 13:47:33 +0200

    A couple more packages and better comments

--- 50_tools.sh
+++ 50_tools.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+# install yarn
 npm install --global yarn
+# install cocoapods
 sudo gem install cocoapods -v 1.11.3
+# install and setup rust (unattended)
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
