9f92699f644edbcb61d723eea892a8a111ef4d7d
Corintho Assunção <corintho.filho@gmail.com>
2022-06-28 13:26:36 +0200

    update key repeat setting

--- 01_settings.sh
+++ 01_settings.sh
@@ -55,7 +55,7 @@ printf "%s\n" "Keyboard - Enable keyboard access for all controls."
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
 printf "%s\n" "Keyboard - Set a fast keyboard repeat rate."
-defaults write NSGlobalDomain KeyRepeat -int 0
+defaults write NSGlobalDomain KeyRepeat -int 2
 
 printf "%s\n" "Keyboard - Disable press-and-hold for keys in favor of key repeat."
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
