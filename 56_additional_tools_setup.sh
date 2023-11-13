f52a4eca9523a87b472c234edc8c893d6eff64a9
Corintho Assuncao <corintho.filho@gmail.com>
2023-11-13 13:47:59 +0100

    feat: add bob to manage neovim versions

--- 56_additional_tools_setup.sh
+++ 56_additional_tools_setup.sh
@@ -3,3 +3,7 @@ mkdir ~/.zfunc
 
 # install zsh completions for additional tools
 bob complete zsh > ~/.zfunc/_bob
+
+# setup bob with the latest stable version of neovim
+bob install stable
+bob use stable
