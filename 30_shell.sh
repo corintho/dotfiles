28cf1aac64554673eac99ac27a85b239c5d62730
Corintho Assuncao <corintho.filho@gmail.com>
2024-05-13 10:07:24 +0200

    fix(tmux): proper setup for tpm

--- 30_shell.sh
+++ 30_shell.sh
@@ -10,5 +10,8 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 #Setup plugins: https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins
 #Setup aliases (cat -> bat), etc
 
+echo "Setup Tmux"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 echo "Installing Powerlevel 10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
