dd84a70f7177011b04f1b343c2c97e1e80a214c5
Corintho Assuncao <corintho.filho@gmail.com>
2024-05-13 09:58:00 +0200

    fix: remove lingering references to oh my tmux

--- 30_shell.sh
+++ 30_shell.sh
@@ -10,12 +10,5 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 #Setup plugins: https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins
 #Setup aliases (cat -> bat), etc
 
-echo "Installing Oh My Tmux"
-cd ~
-git clone https://github.com/gpakosz/.tmux.git
-ln -s -f .tmux/.tmux.conf
-#This is handled in the rc symlink setup
-#cp .tmux/.tmux.conf.local .
-
 echo "Installing Powerlevel 10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
