7ed9def4027fcfa3b5a1369b5c28e06c0893b4c7
Corintho Assunção <github@corintho.eu>
2025-06-17 19:06:13 +0200

    chore: update to use new version of secrets

--- nix/modules/secrets.nix
+++ nix/modules/secrets.nix
@@ -1,6 +1,7 @@
 { secrets, options, paths, ... }: {
   age.identityPaths = options.age.identityPaths.default
     ++ [ "${paths.rootPath}/bootstrap.age" ];
-  age.secrets.smb_corintho.file = "${secrets}/encrypted/smb_corintho.age";
+  age.secrets.smb_corintho.file =
+    "${secrets}/encrypted/personal/smb_corintho.age";
 }
 
