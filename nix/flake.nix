aa12d465ea352731ad917ec3fef80d143b45cc37
Corintho Assunção <github@corintho.eu>
2025-06-18 16:46:04 +0200

    chore: enable shallow cloning and update lock

--- nix/flake.nix
+++ nix/flake.nix
@@ -3,60 +3,61 @@
 
   inputs = {
     # Base packages
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05?shallow=1";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.05?shallow=1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     # Global styling through stylix
     stylix = {
-      url = "github:nix-community/stylix/release-25.05";
+      url = "github:nix-community/stylix/release-25.05?shallow=1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     # Custom pkgs for some modules
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable?shallow=1";
     # Darwin specific packages
     nix-darwin = {
-      url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
+      url = "github:nix-darwin/nix-darwin/nix-darwin-25.05?shallow=1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     # Enable homebrew management through nix
-    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew?shallow=1";
     homebrew-bundle = {
-      url = "github:homebrew/homebrew-bundle";
+      url = "github:homebrew/homebrew-bundle?shallow=1";
       flake = false;
     };
     homebrew-cask = {
-      url = "github:homebrew/homebrew-cask";
+      url = "github:homebrew/homebrew-cask?shallow=1";
       flake = false;
     };
     homebrew-core = {
-      url = "github:homebrew/homebrew-core";
+      url = "github:homebrew/homebrew-core?shallow=1";
       flake = false;
     };
     homebrew-xcodesorg = {
-      url = "github:xcodesorg/homebrew-made";
+      url = "github:xcodesorg/homebrew-made?shallow=1";
       flake = false;
     };
     # Enable agenix for secrets encryption
     agenix = {
-      url = "github:ryantm/agenix";
+      url = "github:ryantm/agenix?shallow=1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     secrets = {
-      url = "git+ssh://git@github.com/corintho/nix-secrets.git?ref=main";
+      url =
+        "git+ssh://git@github.com/corintho/nix-secrets.git?shallow=1&ref=main";
       flake = false;
     };
     # Custom packages for software from other sources
     zen-browser = {
-      url = "github:youwen5/zen-browser-flake";
+      url = "github:youwen5/zen-browser-flake?shallow=1";
 
       # optional, but recommended if you closely follow NixOS unstable so it shares
       # system libraries, and improves startup time
       # NOTE: if you experience a build failure with Zen, the first thing to check is to remove this line!
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    zjstatus = { url = "github:dj95/zjstatus"; };
+    zjstatus = { url = "github:dj95/zjstatus?shallow=1"; };
     # Workaround for absolute path requirements
     paths.url = ./paths;
   };
