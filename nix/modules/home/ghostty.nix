b5f2ee3b409dabccb23681344ef64b0cf1bcf15c
Corintho Assunção <corintho.filho@gmail.com>
2025-07-06 20:51:11 +0200

    chore: change font-size on Darwin

--- nix/modules/home/ghostty.nix
+++ nix/modules/home/ghostty.nix
@@ -7,6 +7,7 @@
         package =
           (if pkgs.stdenv.isDarwin then null else pkgs.unstable.ghostty);
         settings = {
+          font-size = lib.mkForce (if pkgs.stdenv.isDarwin then 14 else 12);
           # Exit without prompt
           confirm-close-surface = false;
           macos-option-as-alt = true;
