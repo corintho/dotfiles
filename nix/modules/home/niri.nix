9e10b1bb7f37e2839e657e72516cd7402d0e4447
Corintho Assunção <github@corintho.eu>
2025-10-20 11:47:47 +0200

    chore: Bazecor launcher override when using niri

--- nix/modules/home/niri.nix
+++ nix/modules/home/niri.nix
@@ -12,4 +12,14 @@
   xdg.configFile = {
     "niri" = { source = config.lib.file.mkOutOfStoreSymlink "${files}/niri"; };
   };
+
+  xdg.desktopEntries = {
+    bazecor = {
+      type = "Application";
+      name = "Bazecor (X11)";
+      exec = "bazecor --ozone-platform=x11 %U";
+      icon = "bazecor";
+      categories = [ "Utility" ];
+    };
+  };
 }
