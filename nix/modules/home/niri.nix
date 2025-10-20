c0215f6236391a2598399cbf05c494ea7d7b3f16
Corintho Assunção <github@corintho.eu>
2025-10-20 17:09:45 +0200

    feat(niri): better setup

--- nix/modules/home/niri.nix
+++ nix/modules/home/niri.nix
@@ -1,7 +1,7 @@
 { config, files, pkgs, ... }:
 
-{
-
+let inherit (config.lib.stylix) colors;
+in {
   # extra packages for niri
   home.packages = with pkgs; [
     unstable.xdg-desktop-portal-gtk
@@ -10,8 +10,18 @@
   ];
 
   xdg.configFile = {
-    "niri" = { source = config.lib.file.mkOutOfStoreSymlink "${files}/niri"; };
+    "niri/layout_colors.kdl".text = ''
+      layout {
+        focus-ring {
+          active-color "#${colors.base07}"
+          active-gradient from="#${colors.base07}" to="#${colors.base0D}" angle=90
+          inactive-color "#${colors.base04}"
+          inactive-gradient from="#${colors.base03}" to="#${colors.base04}" angle=90
+        }
+      }
+    '';
   };
+  xdg.configFile = { "niri/config.kdl" = { source = ./niri/config.kdl; }; };
 
   xdg.desktopEntries = {
     bazecor = {
