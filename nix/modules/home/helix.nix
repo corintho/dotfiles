930e01c76ca2c8bfe71ed47c2a605f391dc5d50d
Corintho Assunção <github@corintho.eu>
2025-10-30 15:33:01 +0100

    chore(helix): configuration updates

--- nix/modules/home/helix.nix
+++ nix/modules/home/helix.nix
@@ -1,10 +1,21 @@
-{ pkgs, ... }: {
+{ lib, pkgs, ... }: {
   config = {
     programs = {
       helix = {
         enable = true;
         package = pkgs.unstable.helix;
+        settings = lib.mkForce {
+          theme = "lcars";
+          editor = { line-number = "relative"; };
+        };
       };
+    };
+    xdg.configFile = {
+      "helix/themes/lcars.toml".text = ''
+        inherits = 'stylix'
+
+        "ui.cursor.primary" = { fg = "base04", modifiers = ["reversed"] }
+      '';
     };
   };
 }
