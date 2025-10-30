6159018a809d0e1b084282a34d915e903a0be201
Corintho Assunção <github@corintho.eu>
2025-10-30 21:12:20 +0100

    chore: helix minimal configuration

--- nix/modules/home/helix.nix
+++ nix/modules/home/helix.nix
@@ -6,7 +6,37 @@
         package = pkgs.unstable.helix;
         settings = lib.mkForce {
           theme = "lcars";
-          editor = { line-number = "relative"; };
+          editor = {
+            # Editor appearance
+            line-number = "relative";
+            bufferline = "multiple";
+            color-modes = true;
+            cursor-shape = {
+              insert = "bar";
+              normal = "block";
+              select = "underline";
+            };
+            indent-guides = { render = true; };
+            # Diagnostics
+            end-of-line-diagnostics = "hint";
+            inline-diagnostics = { cursor-line = "warning"; };
+            # Usability tunings
+            jump-label-alphabet = "ntesiroahdmgcxzlpufywqjbkv";
+            # Input setup
+            mouse = false;
+          };
+          keys = {
+            normal = {
+              space = {
+                q = ":quit";
+                Q = ":quit!";
+                w = ":write";
+                "," = "command_mode";
+              };
+              "]" = { b = ":buffer-next"; };
+              "[" = { b = ":buffer-previous"; };
+            };
+          };
         };
       };
     };
