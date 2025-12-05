0a02c432ec6f46784ba36773cfc99d0aacb1260a
Corintho Assunção <github@corintho.eu>
2025-12-05 18:31:07 +0100

    chore(zed): wrapper for using X11

--- nix/modules/home/zed.nix
+++ nix/modules/home/zed.nix
@@ -1,8 +1,17 @@
-{ pkgs, ... }: 
+{ pkgs, ... }:
 {
+  home.packages = with pkgs; [
+    # Custom scripts on path
+    (writeShellApplication {
+      name = "zed";
+      text = ''
+        env -u WAYLAND_DISPLAY ${pkgs.unstable.zed-editor}/bin/zeditor
+      '';
+    })
+    # /Custom scripts on path
+  ];
   programs.zed-editor = {
     enable = true;
     package = pkgs.unstable.zed-editor;
   };
 }
-
