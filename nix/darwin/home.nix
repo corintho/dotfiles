bb7e3f511de600c4343f3d8c63e1f7dcc2ea126a
Corintho Assunção <corintho.filho@gmail.com>
2025-12-11 22:09:35 +0100

    chore: disable zed on darwin for now

--- nix/darwin/home.nix
+++ nix/darwin/home.nix
@@ -17,7 +17,7 @@ in
     ../modules/home/helix.nix
     ../modules/home/neovim.nix
     ../modules/home/oh_my_posh.nix
-    ../modules/home/zed.nix
+    # ../modules/home/zed.nix
     ../modules/home/zellij.nix
     ../modules/home/darwin
   ]
