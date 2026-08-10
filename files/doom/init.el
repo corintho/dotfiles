dac9396afb198494f99861bee0a326de4558f2b4
Corintho Assunção <github@corintho.eu>
2026-08-10 18:21:46 +0200

    chore(emacs): enable literate config

--- files/doom/init.el
+++ files/doom/init.el
@@ -159,7 +159,7 @@
        nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        ;;odin              ; C, minus its footguns
-       org               ; organize your plain life in plain text
+       (org +pretty)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
@@ -197,5 +197,5 @@
        ;;(rss +org)        ; emacs as an RSS reader
 
        :config
-       ;;literate
+       literate
        (default +bindings +smartparens))
